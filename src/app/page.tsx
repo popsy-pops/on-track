4c028668d6df1b545aaf11c6ce3b198793a12e05
popsy-pops <popsy.pops01@outlook.com>
2026-01-29 11:10:26 -0500

    [+]: home page ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
+import Link from "next/link";
+
 const page = () => {
   return (
-    <div>ok</div>
+    <div className="absolute top-0 left-0 w-screen h-screen flex items-center justify-center">
+      <div className="flex flex-col gap-4">
+        <h1>on track</h1>
+        <p>see your goals achievement on calendar, manage efficiently and get challenging <span>TODO</span>s every day</p>
+        <div className="flex items-center gap-2 justify-start">
+          <Link href="/account/signin" className="px-4 py-1.5 border">signin</Link>
+          <Link href="/account/signup" className="px-4 py-1.5 border">singup</Link>
+        </div>
+      </div>
+    </div>
   );
 };
 export default page;
